c7867fb0759e8f7f4098c2d9cedd03387ff0d462
SpeckNZ <speckcode@gmail.com>
2022-10-24 12:20:33 +1300

    Worked on DisplayListDataFactory

--- Randomizer/obj/Debug/net6.0-windows/Views/MainView.g.i.cs
+++ Randomizer/obj/Debug/net6.0-windows/Views/MainView.g.i.cs
@@ -53,7 +53,7 @@ namespace Randomizer.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Randomizer;component/views/mainview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Randomizer;V1.0.0.0;component/views/mainview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
